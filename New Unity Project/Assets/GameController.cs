65b1cf070fd3995a2d03af1e6095c16cf00b2559
ueno ryo <ru_eon@hotmail.comm>
2018-09-29 17:18:23 +0900

    玉ころがし

---
+++ New Unity Project/Assets/GameController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour
+{
+    public UnityEngine.UI.Text scoreLabel;
+    public GameObject winnerLabelObject;
+
+    public void Update()
+    {
+        int count = GameObject.FindGameObjectsWithTag("Item").Length;
+        scoreLabel.text = count.ToString();
+
+        if (count == 0)
+        {
+            // クリア時の処理
+            // オブジェクトをアクティブにする
+            winnerLabelObject.SetActive(true);
+        }
+    }
+}
